30539075b86839ad11dc0658b63a99ed2589a71f
ian <none@none>
2004-11-08 21:28:28 +0000

    release 0.8.2
    --HG--
    extra : convert_revision : e96139876484d8631dc47575c2809998b3ce111c

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 # Urwid setup.py exports the useful bits
 #    Copyright (C) 2004  Ian Ward
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
+#    This library is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Urwid web site: http://excess.org/urwid/
@@ -23,7 +23,7 @@ from distutils.core import setup
 
 import os
 
-release = "0.8.1" #os.popen("make -s release").read().strip()
+release = "0.8.2" #os.popen("make -s release").read().strip()
 
 setup_d = {
 	'name':"urwid",
@@ -32,7 +32,7 @@ setup_d = {
 	'author_email':"ian@excess.org",
 	'url':"http://excess.org/urwid/",
 	'download_url':"http://excess.org/urwid/urwid-%s.tar.gz"%release,
-	'license':"GPL",
+	'license':"LGPL",
 	'keywords':"curses ui widget scroll listbox interface text layout",
 	'platforms':"unix-like",
 	'description':"A curses-based UI/widget library featuring fluid interface resizing, multiple text layout options, simple markup for attributes, powerful scrolling list boxes and flexible edit boxes.",
@@ -75,7 +75,7 @@ calc.py
 		"Environment :: Console",
 		"Environment :: Console :: Curses",
 		"Intended Audience :: Developers",
-		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
 		"Operating System :: POSIX",
 		"Operating System :: Unix",
 		"Operating System :: MacOS :: MacOS X",
