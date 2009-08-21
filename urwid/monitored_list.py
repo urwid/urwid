437466704ab64cb3ae73102b08f5b86abbc9fd31
Ian Ward <none@none>
2009-08-21 10:49:24 -0400

    untab monitored_list.py

--- urwid/monitored_list.py
+++ urwid/monitored_list.py
@@ -22,36 +22,36 @@
 
 
 def _call_modified(fn):
-	def call_modified_wrapper(self, *args, **kwargs):
-		rval = fn(self, *args, **kwargs)
-		self._modified()
-		return rval
-	return call_modified_wrapper
+    def call_modified_wrapper(self, *args, **kwargs):
+        rval = fn(self, *args, **kwargs)
+        self._modified()
+        return rval
+    return call_modified_wrapper
 
 class MonitoredList(list):
-	def _modified(self):
-		pass
-	
-	def set_modified_callback(self, callback):
-		self._modified = callback
+    def _modified(self):
+        pass
+    
+    def set_modified_callback(self, callback):
+        self._modified = callback
 
-	def __repr__(self):
-		return "%s.%s(%s)" % (self.__class__.__module__,
-			self.__class__.__name__,
-			list.__repr__(self))
+    def __repr__(self):
+        return "%s.%s(%s)" % (self.__class__.__module__,
+            self.__class__.__name__,
+            list.__repr__(self))
 
-	__add__ = _call_modified(list.__add__)
-	__delitem__ = _call_modified(list.__delitem__)
-	__delslice__ = _call_modified(list.__delslice__)
-	__iadd__ = _call_modified(list.__iadd__)
-	__imul__ = _call_modified(list.__imul__)
-	__rmul__ = _call_modified(list.__rmul__)
-	__setitem__ = _call_modified(list.__setitem__)
-	__setslice__ = _call_modified(list.__setslice__)
-	append = _call_modified(list.append)
-	extend = _call_modified(list.extend)
-	insert = _call_modified(list.insert)
-	pop = _call_modified(list.pop)
-	remove = _call_modified(list.remove)
-	reverse = _call_modified(list.reverse)
-	sort = _call_modified(list.sort)
+    __add__ = _call_modified(list.__add__)
+    __delitem__ = _call_modified(list.__delitem__)
+    __delslice__ = _call_modified(list.__delslice__)
+    __iadd__ = _call_modified(list.__iadd__)
+    __imul__ = _call_modified(list.__imul__)
+    __rmul__ = _call_modified(list.__rmul__)
+    __setitem__ = _call_modified(list.__setitem__)
+    __setslice__ = _call_modified(list.__setslice__)
+    append = _call_modified(list.append)
+    extend = _call_modified(list.extend)
+    insert = _call_modified(list.insert)
+    pop = _call_modified(list.pop)
+    remove = _call_modified(list.remove)
+    reverse = _call_modified(list.reverse)
+    sort = _call_modified(list.sort)
