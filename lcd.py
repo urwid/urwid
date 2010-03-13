644babe0607c91d8f9488415d32c718715a5ebe5
Ian Ward <ian@excess.org>
2010-03-13 12:44:51 -0500

    lcd.py example basic menus

--- lcd.py
+++ lcd.py
@@ -49,22 +49,95 @@ def program_cgram(screen):
         screen.program_cgram(num, cdata)
 
 
+class LCDCheckBox(urwid.CheckBox):
+    """
+    A check box+label that uses only one character for the check box
+    """
+    states = {
+        True: urwid.SelectableIcon('\xd0', cursor_position=0),
+        False: urwid.SelectableIcon('\x05', cursor_position=0),
+    }
+    reserve_columns = 1
 
-txt = urwid.Edit("",
-    "hello\n\x11\x03\x00\x02\x01\x00\x04\n\xd0\x05\xbb\x06\x07", 
-    align=urwid.CENTER, multiline=True)
+class LCDRadioButton(urwid.RadioButton):
+    """
+    A radio button+label that uses only one character for the radio button
+    """
+    states = {
+        True: urwid.SelectableIcon('\xbb', cursor_position=0),
+        False: urwid.SelectableIcon('\x06', cursor_position=0),
+    }
+    reserve_columns = 1
+
+class MenuOption(urwid.Button):
+    """
+    A menu option, indicated with a single arrow character
+    """
+    def __init__(self, label, submenu):
+        self.__super.__init__("")
+        # use a Text widget for label, we want the cursor
+        # on the arrow not the label
+        self._label = urwid.Text("")
+        self.set_label(label)
+
+        self._w = urwid.Columns([
+            ('fixed', 1, urwid.SelectableIcon('\xdf', cursor_position=0)),
+            self._label])
+
+        urwid.connect_signal(self, 'click', 
+            lambda option: show_menu(submenu))
+
+
+class Menu(urwid.ListBox):
+    def __init__(self, widgets):
+        self.menu_parent = None
+        self.__super.__init__(urwid.SimpleListWalker(widgets))
+
+    def keypress(self, size, key):
+        """
+        Go back to the previous menu on cancel button (mapped to esc)
+        """
+        if key == 'esc' and self.menu_parent:
+            show_menu(self.menu_parent)
+        else:
+            return self.__super.keypress(size, key)
+
+def build_menus():
+    cursor_option_group = []
+
+    m_display = Menu([
+        LCDCheckBox('check'),
+        LCDCheckBox('it out'),
+        ])
+    m_cursor = Menu([
+        LCDRadioButton(cursor_option_group, "one"),
+        LCDRadioButton(cursor_option_group, "two"),
+        LCDRadioButton(cursor_option_group, "three"),
+        ])
+    m_leds = Menu([])
+    m_about = Menu([urwid.Text("some about text")])
+
+    o_display = MenuOption('Display Settings', m_display)
+    o_cursor = MenuOption('Cursor Settings', m_cursor)
+    o_leds = MenuOption('LEDs', m_leds)
+    o_about = MenuOption('About this Demo', m_about)
+
+    m_root = Menu([o_display, o_cursor, o_leds, o_about])
+    m_display.menu_parent = m_cursor.menu_parent = m_root
+    m_leds.menu_parent = m_about.menu_parent = m_root
+
+    return m_root
+
 screen = urwid.lcd_display.CF635Screen('/dev/ttyUSB0')
+# set up our font
 program_cgram(screen)
+loop = urwid.MainLoop(build_menus(), screen=screen)
 # FIXME: want screen to know it is in narrow mode, or better yet, 
 # do the unicode conversion for us
 urwid.set_encoding('narrow')
 
-
-def unhandled(i):
-    print i
-
-loop = urwid.MainLoop(urwid.Filler(txt), 
-    screen=screen,
-    unhandled_input=unhandled)
+def show_menu(menu):
+    loop.widget = menu
 
 loop.run()
+
