733270c35a6def5cd841aaa1054ba0b165c7e660
aszlig <aszlig@redmoonstudios.org>
2010-06-29 21:12:46 +0200

    vterm: Added missing copyright headers.

--- urwid/vterm_test.py
+++ urwid/vterm_test.py
@@ -1,3 +1,24 @@
+#!/usr/bin/python
+#
+# Urwid terminal emulation widget unit tests
+#    Copyright (C) 2010  aszlig
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Urwid web site: http://excess.org/urwid/
+
 import os
 import sys
 import unittest
