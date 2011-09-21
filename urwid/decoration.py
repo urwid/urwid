38226a4f0f6129a736578758f88a094e0780bcb4
Ian Ward <ian@excess.org>
2011-09-21 00:30:40 -0400

    WidgetPlaceholder: a do-nothing decoration

--- urwid/decoration.py
+++ urwid/decoration.py
@@ -87,6 +87,21 @@ class WidgetDecoration(Widget):  # "decorator" was already taken
         return self._original_widget.sizing()
 
 
+class WidgetPlaceholder(delegate_to_widget_mixin('_original_widget'),
+        WidgetDecoration):
+    """
+    This is a do-nothing decoration widget that can be used for swapping
+    between widgets without modifying the container of this widget.
+
+    This can be useful for making an interface with a number of distinct
+    pages or for showing and hiding menu or status bars.
+
+    The widget displayed is stored as a .original_widget property and
+    can be changed by assigning a new widget to it.
+    """
+    pass
+
+
 class AttrMapError(WidgetError):
     pass
 
