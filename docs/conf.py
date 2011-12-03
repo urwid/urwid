75e79947c8bc4ada72d3dfaa8ef10f2ed631a94a
marco giusti <marco.giusti@gmail.com>
2011-12-03 12:59:15 +0100

    Fix static and templates paths
    --HG--
    branch : feature-sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ import sys, os
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['tools']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
@@ -120,7 +120,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['tools/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
