d3ad7b0112bdb87b2b69c69f9b3623c27e8c096e
Ian Ward <ian@excess.org>
2012-10-17 10:08:51 -0400

    docs: readthedocs theme setting

--- docs/conf.py
+++ docs/conf.py
@@ -96,6 +96,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
+html_style = None # make readthedocs really use the default theme
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
