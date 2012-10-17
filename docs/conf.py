75f296bc13449081532abc144b423367299ef5ad
Ian Ward <ian@excess.org>
2012-10-16 22:05:51 -0400

    docs: urwid colors and logo

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,16 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'sidebarbgcolor':'#263193',
+    'footerbgcolor':'#2a1b5c',
+    'relbarbgcolor':'#2a1b5c',
+    'sidebarlinkcolor':'#aabee8',#'#6d96e8',
+    'linkcolor':'#263193',
+    'visitedlinkcolor':'#263193',
+    'headtextcolor':'#2a1b5c',
+    'headlinkcolor':'#2a1b5c',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -114,7 +123,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'urwid-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
