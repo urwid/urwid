ad18da56e119377b1a37191294a01735523dc5cd
Ian Ward <ian@excess.org>
2012-10-20 16:05:38 -0400

    tutoral: cmenu.py make cascade max assumption explicit

--- docs/tutorial/cmenu.py
+++ docs/tutorial/cmenu.py
@@ -40,20 +40,23 @@ menu_top = menu(u'Main Menu', [
 ])
 
 class CascadingBoxes(urwid.WidgetPlaceholder):
+    max_box_levels = 4
+
     def __init__(self, box):
         super(CascadingBoxes, self).__init__(urwid.SolidFill(u'/'))
         self.box_level = 0
         self.open_box(box)
 
     def open_box(self, box):
-        # NOTE: assumes we'll only ever have 4 boxes
         self.original_widget = urwid.Overlay(urwid.LineBox(box),
             self.original_widget,
             align='center', width=('relative', 80),
             valign='middle', height=('relative', 80),
             min_width=24, min_height=8,
-            left=self.box_level * 3, right=(3 - self.box_level) * 3,
-            top=self.box_level * 2, bottom=(3 - self.box_level) * 2)
+            left=self.box_level * 3,
+            right=(self.max_box_levels - self.box_level - 1) * 3,
+            top=self.box_level * 2,
+            bottom=(self.max_box_levels - self.box_level - 1) * 2)
         self.box_level += 1
 
     def keypress(self, size, key):
