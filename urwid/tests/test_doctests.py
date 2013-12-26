449cf902a327b17f432d81e323924bcac5aa78b7
Ian Ward <ian@excess.org>
2013-12-26 10:58:13 -0500

    fix setup.py test to run doctests again

---
+++ urwid/tests/test_doctests.py
@@ -0,0 +1,21 @@
+import unittest
+import doctest
+
+import urwid
+
+def load_tests(loader, tests, ignore):
+    module_doctests = [
+        urwid.widget,
+        urwid.wimp,
+        urwid.decoration,
+        urwid.display_common,
+        urwid.main_loop,
+        urwid.monitored_list,
+        urwid.raw_display,
+        'urwid.split_repr', # override function with same name
+        urwid.util,
+        ]
+    for m in module_doctests:
+        tests.addTests(doctest.DocTestSuite(m,
+            optionflags=doctest.ELLIPSIS | doctest.IGNORE_EXCEPTION_DETAIL))
+    return tests
