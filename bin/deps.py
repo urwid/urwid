5482dae93483a757224d2ab913517b31085878de
Ian Ward <ian@excess.org>
2013-12-27 10:05:03 -0500

    travis: show optional deps

---
+++ bin/deps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+show optional Urwid dependencies installed
+"""
+deps = []
+
+try:
+    import gi.repository
+    deps.append("pygobject")
+except ImportError:
+    pass
+
+try:
+    import tornado
+    deps.append("tornado")
+except ImportError:
+    pass
+
+try:
+    import twisted
+    deps.append("twisted")
+except ImportError:
+    pass
+
+print(" ".join(deps))
