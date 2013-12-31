a3f4d48d014f6aa5678bf95d2395b81813e7eb60
Ian Ward <ian@excess.org>
2013-12-31 13:41:22 -0500

    move long_description to README.rst

---
+++ README.rst
@@ -0,0 +1,16 @@
+Urwid is a console user interface library for Python.
+It includes many features useful for text console application developers including:
+
+- Applcations resize quickly and smoothly
+- Automatic, programmable text alignment and wrapping
+- Simple markup for setting text attributes within blocks of text
+- Powerful list box with programmable content for scrolling all widget types
+- Your choice of event loops: Twisted, Glib or built-in select-based loop
+- Pre-built widgets include edit boxes, buttons, check boxes and radio buttons
+- Display modules include raw, curses, and experimental LCD and web displays
+- Support for UTF-8, simple 8-bit and CJK encodings
+- 256 and 88 color mode support
+- Compatible with Python 2.4, 2.5, 2.6, 2.7, 3.2 and 3.3
+
+Home Page:
+  http://urwid.org/
--- setup.py
+++ setup.py
@@ -50,24 +50,7 @@ setup_d = {
     'keywords':"curses ui widget scroll listbox user interface text layout console ncurses",
     'platforms':"unix-like",
     'description': "A full-featured console (xterm et al.) user interface library",
-    'long_description':"""
-Urwid is a console user interface library.  It includes many features
-useful for text console application developers including:
-
-- Applcations resize quickly and smoothly
-- Automatic, programmable text alignment and wrapping
-- Simple markup for setting text attributes within blocks of text
-- Powerful list box with programmable content for scrolling all widget types
-- Your choice of event loops: Twisted, Glib or built-in select-based loop
-- Pre-built widgets include edit boxes, buttons, check boxes and radio buttons
-- Display modules include raw, curses, and experimental LCD and web displays
-- Support for UTF-8, simple 8-bit and CJK encodings
-- 256 and 88 color mode support
-- Compatible with Python 2.4, 2.5, 2.6, 2.7, 3.2 and 3.3
-
-Home Page:
-  http://urwid.org/
-"""[1:],
+    'long_description':open("README.rst").read(),
     'classifiers':[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
