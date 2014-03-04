e6efc7137b8d6430f2972e8ed0b73b962341a95f
Chipster <julien.chipster@gmail.com>
2014-03-04 18:05:06 +0100

    Solve a bug: 'utf-8' codec can't decode byte 0xc3
    UnicodeDecodeError: 'utf-8' codec can't decode byte 0xc3 in position 12: unexpected end of data

--- urwid/raw_display.py
+++ urwid/raw_display.py
@@ -765,7 +765,7 @@ class Screen(BaseScreen, RealTerminal):
         try:
             for l in o:
                 if isinstance(l, bytes) and PYTHON3:
-                    l = l.decode('utf-8')
+                    l = l.decode('utf-8', 'ignore')
                 self._term_output_file.write(l)
             self._term_output_file.flush()
         except IOError, e:
