ede0b0b81ce6b8f8adcfe332d58764b3818549bd
Tony Cebzanov <tonycpsu@gmail.com>
2019-04-05 12:28:44 -0400

    Include traceback on coroutine exception as discussed in #235, #283.

--- urwid/main_loop.py
+++ urwid/main_loop.py
@@ -1470,7 +1470,7 @@ class AsyncioEventLoop(EventLoop):
             loop.stop()
             if not isinstance(exc, ExitMainLoop):
                 # Store the exc_info so we can re-raise after the loop stops
-                self._exc_info = (type(exc), exc, None)
+                self._exc_info = (type(exc), exc, exc.__traceback__)
         else:
             loop.default_exception_handler(context)
 
