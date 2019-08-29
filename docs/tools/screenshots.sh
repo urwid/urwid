bda1bcce224b0459fd860ae18ecbd7eac9fa7a81
max ulidtko <ulidtko@gmail.com>
2019-08-29 18:15:14 +0300

    Fix(docs): screenshot rendering race-condition
    Fixes #101 and #102.
    
    The issue is rather simple:
     * we run an example in a virtual X display,
     * we send events to an example via xdotool,
     * immediately after, we take a screenshot (with ImageMagick).
    
    We don't ever synchronize to a point "all X11 events have
    been pumped, handled, and the example has redrawn" -- so there's an
    obvious race happening.
    
    Fix the race by locating the PID of the example interpreter process,
    (it'll be the only child of our urxvt instance),
    and polling its process status (running or not). When we detect it's
    not running N times in a row -- conclude it's done and went idling.
    With increasing N, the total probability of misdetection drops
    exponentially.
    
    N=10 should suffice as a rough 1st guess; if the race reappears --
    simply increase it a couple of points, say to 15 or 16.

--- docs/tools/screenshots.sh
+++ docs/tools/screenshots.sh
@@ -19,11 +19,33 @@ done
 export RXVTWINDOWID
 image=${1%.py}
 
+# Synchronize to a point when the example has finished rendering.
+# Once in n consecutive polls we don't see any "R (running)", consider that the
+# process has completed the initial rendering & processing xdotool events,
+# and has become idle.
+# With increasing n, the probability of misdetection falls exponentially.
+sync_example_settled() {
+	pid=$1
+	n=0
+	while (( n < 10 )); do
+		sleep 0.010
+		procstate="$(cut -d' ' -f3 < /proc/"$pid"/stat)"
+		case $procstate in
+			R) n=0;;
+			*) _=$(( ++n ));;
+		esac
+	done
+}
+
+# urxvt will have an interpreter as one child process; find it
+example_pid="$(ps --no-headers -o pid --ppid $RXVTPID)"
+
 c=1
 while read -r line; do
 	# the echo trick is needed to expand RXVTWINDOWID variable
 	echo $line | xdotool - 2>/dev/null
 	echo "sending $line"
+	sync_example_settled "$example_pid"
 	import -window "$RXVTWINDOWID" "${image}$c.png"
 	(( c++ ))
 done
