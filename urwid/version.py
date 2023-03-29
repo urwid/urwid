3b61266f91616cd0ec80b3a16ef6c0367fadd60a
Alexey Stepanov <penguinolog@users.noreply.github.com>
2023-03-29 09:58:00 +0200

    Test fixes (#524)
    * asyncio coroutine was not awaited, so test was false-positive (poor visible on Travis CI)
      Fix test.
    * FloatEdit unittests passed, incorrect output doctests not caused exception on CI
    * warnings on version number
    * warnings about deprecated setup.py usage for tests run
    * python2.7 is not able to execute tests for asyncio due to mandatory syntax changes (async def).
      Fix logic:
        1. added `isinstance` check before getting significance
        2. Fix FloatEdit and add corner case tests
        3. normalize version number
        4. execute tests via `unittest`
        5. stop using python 2.7 in tests run instead of increasing complexity
    
    Co-authored-by: Aleksei Stepanov <alekseis@nvidia.com>

--- urwid/version.py
+++ urwid/version.py
@@ -1,4 +1,4 @@
 from __future__ import division, print_function
 
-VERSION = (2, 1, 3, 'dev')
-__version__ = ''.join(['-.'[type(x) == int]+str(x) for x in VERSION])[1:]
+VERSION = (2, 1, 3, 'dev0')
+__version__ = '.'.join([str(x) for x in VERSION])
