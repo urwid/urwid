9e7ec1c69d33c7b365b081ca72074e6c6979c85f
Alexey Stepanov <penguinolog@users.noreply.github.com>
2023-03-29 09:58:00 +0200

    Test fixes (#524)
    * asyncio coroutine was not awaited, so test was false-positive (poor visible on Travis CI)
      Fix test.
    * FloatEdit unittests passed, incorrect output doctests not caused exception on CI
    * warnings on version number
    * warnings about deprecated setup.py usage for tests run
    * python2.7 is not able to execute tests for asyncio due to mandatory syntax changes (async def).
      Fix logic:
        1. added `isinstance` check before getting significance
        2. Fix FloatEdit and add corner case tests
        3. normalize version number
        4. execute tests via `unittest`
        5. stop using python 2.7 in tests run instead of increasing complexity
    
    Co-authored-by: Aleksei Stepanov <alekseis@nvidia.com>

--- urwid/tests/test_floatedit.py
+++ urwid/tests/test_floatedit.py
@@ -1,21 +1,41 @@
 import unittest
 from urwid.numedit import FloatEdit
-from decimal import Decimal, getcontext
+from decimal import Decimal
 from itertools import product
+
+
 class FloatEditNoPreservePrecicionTest(unittest.TestCase):
     def test(self):
         for v in ('1.01', '2.5', '300', '4.100', '5.001'):
             f = FloatEdit(preserveSignificance=False)
             f.set_edit_text(v)
             print(f.value(), Decimal(v))
-            assert f.value() == Decimal(v), f'{f.value()} != {Decimal(v)}'
+            self.assertEqual(Decimal(v), f.value())
+
+    def test_int(self):
+        corner_value = "1"  # just int
+        f = FloatEdit(preserveSignificance=False)
+        f.set_edit_text(corner_value)
+        self.assertEqual(Decimal(corner_value), f.value())
+
+    def test_no_post_dot(self):
+        corner_value = "1."  # forced float 1.0 with trimmed end
+        f = FloatEdit(preserveSignificance=False)
+        f.set_edit_text(corner_value)
+        self.assertEqual(Decimal(corner_value), f.value())
+
 
 class FloatEditPreservePrecicionTest(unittest.TestCase):
 
     def test(self):
-        for v, sig, sep in product(('1.010', '2.500', '300.000', '4.100', '5.001'), (1, 2, 3), ('.', ',')):
-            f = FloatEdit(preserveSignificance=True, default='0.' + '0'*sig, decimalSeparator=sep)
+        for v, sig, sep in product(('1.010', '2.500', '300.000', '4.100', '5.001'), (0, 1, 2, 3), ('.', ',')):
+            precision_template = '0.' + '0'*sig
+            expected = Decimal(v).quantize(Decimal(precision_template))
+            f = FloatEdit(preserveSignificance=True, default=precision_template, decimalSeparator=sep)
+
             f.set_edit_text(v.replace('.', sep))
-            getcontext().prec = sig+1
-            print(f.value(), Decimal(v))
-            assert str(f.value()) == str(Decimal(v) * 1), f'{str(f.value())} != {str(Decimal(v)*1)} (significance={sig})'
+            self.assertEqual(
+                expected,
+                f.value(),
+                f'{f.value()} != {expected} (significance={sig!r})'
+            )
