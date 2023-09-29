79bda76e1461125f91a073e472ed6ee5301369d3
max ulidtko <ulidtko@gmail.com>
2023-09-29 22:35:14 +0300

    Fix(docs): unstub subproc2 example
    This was stubbed with a dumb print... But now, the CPU-nontrivial
    example can actually correctly render.

--- docs/examples/subproc2.py
+++ docs/examples/subproc2.py
@@ -1,12 +1,1 @@
-from __future__ import annotations
-
-import time
-
-time.sleep(1)
-
-print("""factor: 1
-factor: 1000003
-factor: 2000029
-factor: 3000073
-factor: 4000159
-factor: 5000101""")
+../../examples/subproc2.py
