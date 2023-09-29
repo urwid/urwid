d93b759f8142b10cafae226f630a6021ea5c5427
max ulidtko <ulidtko@gmail.com>
2023-09-29 18:02:51 +0300

    Fix(docs): use ImageMagick to compare by pixels, not PNG bytes

--- docs/tools/screenshot-race-stresser.sh
+++ docs/tools/screenshot-race-stresser.sh
@@ -2,8 +2,10 @@
 
 cd "$(dirname "$0")"/.. || die
 
-counter=100
-until [[ $counter -eq 0 ]]; do
+rm -rf examples.ref; mkdir -vp examples.ref; cp -v examples/*.png examples.ref
+
+counter=1
+until [[ $counter -eq 100 ]]; do
 
     echo
     echo
@@ -12,13 +14,18 @@ until [[ $counter -eq 0 ]]; do
     echo "================================================================================"
     tools/compile_pngs.sh examples/*.py
 
-    if ! git diff --quiet ./\*\*.png; then
-        git status
-        echo "changes detected, aborting stresstest!"
-        exit 1
-    fi
+    for r in examples.ref/*.png; do
+        n=$(basename $r)
+        t=examples/$n
+        d=/tmp/diff-$n
+        #-- use compare from ImageMagick
+        if ! compare $r $t $d; then
+            echo "changes detected, aborting stresstest! see $d"
+            exit 1
+        fi
+    done
 
-    : $((--counter))
+    : $((++counter))
 done
 
 exit 0
