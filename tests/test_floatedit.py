f26bd6798315aaa811c9869744ebaece3dae65af
Alexey Stepanov <penguinolog@users.noreply.github.com>
2024-01-22 11:36:46 +0100

    Deduplicate code in `frame` module (#759)
    * `WidgetContainerMixin` & `WidgetContainerListContentsMixin`
      copy/paste with `container module`
    * `FrameContents` subclasses `MutableMapping`
      do not make useless overloads
    
    Unrelated: partial auto-format of functional tests
    
    Co-authored-by: Aleksei Stepanov <alekseis@nvidia.com>

--- tests/test_floatedit.py
+++ tests/test_floatedit.py
@@ -7,7 +7,7 @@ from urwid.numedit import FloatEdit
 
 class FloatEditNoPreservePrecicionTest(unittest.TestCase):
     def test(self):
-        for v in ('1.01', '2.5', '300', '4.100', '5.001'):
+        for v in ("1.01", "2.5", "300", "4.100", "5.001"):
             f = FloatEdit(preserveSignificance=False)
             f.set_edit_text(v)
             print(f.value(), Decimal(v))
@@ -27,16 +27,11 @@ class FloatEditNoPreservePrecicionTest(unittest.TestCase):
 
 
 class FloatEditPreservePrecicionTest(unittest.TestCase):
-
     def test(self):
-        for v, sig, sep in product(('1.010', '2.500', '300.000', '4.100', '5.001'), (0, 1, 2, 3), ('.', ',')):
-            precision_template = '0.' + '0'*sig
+        for v, sig, sep in product(("1.010", "2.500", "300.000", "4.100", "5.001"), (0, 1, 2, 3), (".", ",")):
+            precision_template = "0." + "0" * sig
             expected = Decimal(v).quantize(Decimal(precision_template))
             f = FloatEdit(preserveSignificance=True, default=precision_template, decimalSeparator=sep)
 
-            f.set_edit_text(v.replace('.', sep))
-            self.assertEqual(
-                expected,
-                f.value(),
-                f'{f.value()} != {expected} (significance={sig!r})'
-            )
+            f.set_edit_text(v.replace(".", sep))
+            self.assertEqual(expected, f.value(), f"{f.value()} != {expected} (significance={sig!r})")
--- tests/test_signals.py
+++ tests/test_signals.py
@@ -6,24 +6,23 @@ from urwid import Edit, Signals, connect_signal, disconnect_signal, emit_signal,
 
 
 class SiglnalsTest(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        cls.EmClass = type("EmClass", (object, ), {})
+        cls.EmClass = type("EmClass", (object,), {})
         register_signal(cls.EmClass, ["change", "test"])
 
     def test_connect(self):
         obj = Mock()
         handler = Mock()
-        edit = Edit('')
-        key = connect_signal(edit, 'change', handler, user_args=[obj])
+        edit = Edit("")
+        key = connect_signal(edit, "change", handler, user_args=[obj])
         self.assertIsNotNone(key)
-        edit.set_edit_text('long test text')
-        handler.assert_called_once_with(obj, edit, 'long test text')
+        edit.set_edit_text("long test text")
+        handler.assert_called_once_with(obj, edit, "long test text")
 
         handler.reset_mock()
-        disconnect_signal(edit, 'change', handler, user_args=[obj])
-        edit.set_edit_text('another text')
+        disconnect_signal(edit, "change", handler, user_args=[obj])
+        edit.set_edit_text("another text")
         handler.assert_not_called()
 
     @unittest.skipIf(sys.implementation.name == "pypy", "WeakRef works differently on PyPy")
@@ -36,30 +35,30 @@ class SiglnalsTest(unittest.TestCase):
         handler1 = Mock(name="handler1")
         handler2 = Mock(name="handler2")
 
-        k1 = connect_signal(emitter, 'test', handler1, weak_args=[w1], user_args=[42, "abc"])
-        k2 = connect_signal(emitter, 'test', handler2, weak_args=[w2, w3], user_args=[8])
+        k1 = connect_signal(emitter, "test", handler1, weak_args=[w1], user_args=[42, "abc"])
+        k2 = connect_signal(emitter, "test", handler2, weak_args=[w2, w3], user_args=[8])
         self.assertIsNotNone(k2)
 
-        emit_signal(emitter, 'test', "Foo")
-        handler1.assert_called_once_with(w1, 42, "abc",  "Foo")
+        emit_signal(emitter, "test", "Foo")
+        handler1.assert_called_once_with(w1, 42, "abc", "Foo")
         handler2.assert_called_once_with(w2, w3, 8, "Foo")
 
         handler1.reset_mock()
         handler2.reset_mock()
         del w1
         self.assertEqual(
-            len(getattr(emitter, Signals._signal_attr)['test']),
+            len(getattr(emitter, Signals._signal_attr)["test"]),
             1,
-            getattr(emitter, Signals._signal_attr)['test']
+            getattr(emitter, Signals._signal_attr)["test"],
         )
-        emit_signal(emitter, 'test', "Bar")
+        emit_signal(emitter, "test", "Bar")
         handler1.assert_not_called()
         handler2.assert_called_once_with(w2, w3, 8, "Bar")
 
         handler2.reset_mock()
         del w3
-        emit_signal(emitter, 'test', "Baz")
+        emit_signal(emitter, "test", "Baz")
         handler1.assert_not_called()
         handler2.assert_not_called()
-        self.assertEqual(len(getattr(emitter, Signals._signal_attr)['test']), 0)
+        self.assertEqual(len(getattr(emitter, Signals._signal_attr)["test"]), 0)
         del w2
