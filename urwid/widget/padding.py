71df2dcd2c5c8babe5bb229c5a557fb51395fd1c
Aleksei Stepanov <alekseis@nvidia.com>
2024-12-03 18:16:05 +0100

    Remove deprecated protected getter methods from the decoration widgets
    * `WidgetDecoration._get_original_widget`
    * `WidgetDecoration._get_base_widget`
    
    * `Padding._get_align`
    * `Padding._get_width`

--- urwid/widget/padding.py
+++ urwid/widget/padding.py
@@ -215,15 +215,6 @@ class Padding(WidgetDecoration[WrappedWidget], typing.Generic[WrappedWidget]):
         self._align_type, self._align_amount = normalize_align(align, PaddingError)
         self._invalidate()
 
-    def _get_align(self) -> Literal["left", "center", "right"] | tuple[Literal["relative"], int]:
-        warnings.warn(
-            f"Method `{self.__class__.__name__}._get_align` is deprecated, "
-            f"please use property `{self.__class__.__name__}.align`",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return self.align
-
     def _set_align(self, align: Literal["left", "center", "right"] | tuple[Literal["relative"], int]) -> None:
         warnings.warn(
             f"Method `{self.__class__.__name__}._set_align` is deprecated, "
@@ -260,15 +251,6 @@ class Padding(WidgetDecoration[WrappedWidget], typing.Generic[WrappedWidget]):
         """
         self._width_type, self._width_amount = normalize_width(width, PaddingError)
         self._invalidate()
-
-    def _get_width(self) -> Literal["clip", "pack"] | int | tuple[Literal["relative"], int]:
-        warnings.warn(
-            f"Method `{self.__class__.__name__}._get_width` is deprecated, "
-            f"please use property `{self.__class__.__name__}.width`",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return self.width
 
     def _set_width(self, width: Literal["clip", "pack"] | int | tuple[Literal["relative"], int]) -> None:
         warnings.warn(
