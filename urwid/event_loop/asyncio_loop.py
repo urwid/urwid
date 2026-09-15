8c28e87ee1aa1df5d53a063843f64aa182cb62ed
Aleksei Stepanov <penguinolog@users.noreply.github.com>
2026-09-15 16:48:35 +0200

    Support async callbacks in AsyncioEventLoop (#1299)
    Fix #310

--- urwid/event_loop/asyncio_loop.py
+++ urwid/event_loop/asyncio_loop.py
@@ -25,6 +25,7 @@ from __future__ import annotations
 
 import asyncio
 import functools
+import inspect
 import logging
 import sys
 import typing
@@ -61,6 +62,11 @@ class AsyncioEventLoop(EventLoop):
 
         A good way to do this:
             asyncio.get_event_loop().call_soon(main_loop.draw_screen)
+
+    .. note::
+        :meth:`alarm`, :meth:`watch_file` and :meth:`enter_idle` accept an ``async def``
+        callback in addition to a plain callable. A coroutine function is scheduled as
+        an ``asyncio.Task`` instead of being called directly.
     """
 
     def __init__(self, *, loop: asyncio.AbstractEventLoop | None = None, **kwargs: typing.Any) -> None:
@@ -105,6 +111,7 @@ class AsyncioEventLoop(EventLoop):
         self._idle_asyncio_handle: asyncio.TimerHandle | None = None
         self._idle_handle: int = 0
         self._idle_callbacks: dict[int, Callable[[], typing.Any]] = {}
+        self._background_tasks: set[asyncio.Task[typing.Any]] = set()
 
     def __del__(self) -> None:
         if sys.version_info[:2] < (3, 11):
@@ -113,18 +120,37 @@ class AsyncioEventLoop(EventLoop):
         elif self._runner is not None:
             self._runner.close()
 
-    def _also_call_idle(self, callback: Callable[_Spec, _T]) -> Callable[_Spec, _T]:
+    def _also_call_idle(self, callback: Callable[_Spec, _T]) -> Callable[_Spec, _T | None]:
         """
         Wrap the callback to also call _entering_idle.
         """
 
         @functools.wraps(callback)
-        def wrapper(*args: _Spec.args, **kwargs: _Spec.kwargs) -> _T:
+        def wrapper(*args: _Spec.args, **kwargs: _Spec.kwargs) -> _T | None:
             if not self._idle_asyncio_handle:
                 self._idle_asyncio_handle = self._loop.call_later(0, self._entering_idle)
-            return callback(*args, **kwargs)
+            return self._run_callback(callback, *args, **kwargs)
 
         return wrapper
+
+    def _run_callback(self, callback: Callable[_Spec, _T], *args: _Spec.args, **kwargs: _Spec.kwargs) -> _T | None:
+        """Call callback, scheduling it as a task instead if it is a coroutine function.
+
+        :param callback: function or coroutine function to call
+        :type callback: Callable
+        :param args: positional arguments to pass to callback
+        :type args: object
+        :param kwargs: keyword arguments to pass to callback
+        :type kwargs: object
+        :return: callback return value, or None if it was scheduled as a background task
+        :rtype: object | None
+        """
+        if inspect.iscoroutinefunction(callback):
+            task = self._loop.create_task(callback(*args, **kwargs))
+            self._background_tasks.add(task)
+            task.add_done_callback(self._background_tasks.discard)
+            return None
+        return callback(*args, **kwargs)
 
     def _entering_idle(self) -> None:
         """
@@ -132,7 +158,7 @@ class AsyncioEventLoop(EventLoop):
         """
         try:
             for callback in self._idle_callbacks.values():
-                callback()
+                self._run_callback(callback)
         finally:
             self._idle_asyncio_handle = None
 
